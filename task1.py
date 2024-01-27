448e343509d22202c456ff2675cf916c13da3c86
Danylo Dubok <danildubok77@gmail.com>
2024-01-27 13:35:38 +0200

    1

---
+++ task1.py
@@ -0,0 +1,45 @@
+# int
+x = 5
+#int: Представляє цілі числа без десяткової частини.
+
+# float
+y = 3.14
+#float: Представляє числа з плаваючою комою (десяткові числа).
+
+
+
+#Конвертація між int та float:
+# int to float
+int_num = 10
+float_num = float(int_num)
+
+# float to int
+float_num = 7.5
+int_num = int(float_num)
+
+
+
+#Приклад використання:
+# Числа int
+a = 10
+b = 20
+
+# Числа float
+c = 3.5
+d = 2.0
+
+# Додавання int та float
+result1 = a + c  # Результат буде float (13.5)
+
+# Множення int та float
+result2 = b * d  # Результат буде float (40.0)
+
+# Конвертація int to float
+float_a = float(a)  # float_a = 10.0
+
+# Конвертація float to int
+int_c = int(c)  # int_c = 3
+
+# Ділення int на float
+result3 = b / d  # Результат буде float (10.0)
+# test
